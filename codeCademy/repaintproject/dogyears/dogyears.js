a4d0e6b74e028766b9a2b8017a6c093b559dc35f
pedro tesch <pedrotesch.pt@gmail.com>
2022-12-22 17:30:47 -0300

    nothing too much

---
+++ codeCademy/repaintproject/dogyears/dogyears.js
@@ -0,0 +1,14 @@
+let myAge = 22; //this is my age
+let earlyYears = 2; //i really don't know what this means
+earlyYears *= 10.5;
+console.log(earlyYears);
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(laterYears);
+
+let myAgeInDogYears = laterYears + earlyYears;
+
+let myName = "PEdro".toLowerCase();
+
+console.log("My name is", myName, ". I am ", myAge, " years old in human years which is", myAgeInDogYears, "years old in dog years")

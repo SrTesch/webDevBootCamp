649324cea72c9aabb53e283818b9de44b492560f
pedro tesch <pedrotesch.pt@gmail.com>
2023-02-17 17:37:48 -0300

    starting with basics of react

---
+++ codeCademy/node/destructuring.js
@@ -0,0 +1,13 @@
+//destructuring arrays
+let colors = ['yellow', 'white', 'black'];
+let [color1, color2, color3] = colors;
+
+console.log(color1, color2, color3);
+
+
+
+//destructuring objects
+let destinations = { x: 'LA', y: 'NYC', z: 'MIA' };
+let { x, y, z } = destinations;
+
+console.log(x, y, z);
---
+++ codeCademy/react/trying.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const myDiv = (
+    <div>
+        <h1>Eu sou lindo!</h1>
+    </div>
+)
